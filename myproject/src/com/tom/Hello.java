6e19515cf6e3c1bed9dae45b7bc8099f2d53763c
user <user@es302n07>
2018-09-25 11:58:09 +0800

    initial

---
+++ myproject/src/com/tom/Hello.java
@@ -0,0 +1,13 @@
+package com.tom;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int age = 19;
+        System.out.println(age);
+        float weight = 45.5f;
+        System.out.println(weight);
+	}
+
+}
